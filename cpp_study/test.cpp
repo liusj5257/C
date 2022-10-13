8a25dd981ef3fe986e2322744e6c712a0a945aad
aqeqqq <liu-shaojun@outlook.com>
2022-10-13 16:49:23 +0800

    test

--- cpp_study/test.cpp
+++ cpp_study/test.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 
-// #include "std.hpp"
+// ！include "std.hpp"
 
 int main() {
   using namespace std;
-
   cout << "c++ version = " << __cplusplus << endl;
-
   cout << "gcc version = " << __VERSION__ << endl;
-
   cout << "gcc major = " << __GNUC__ << endl;
   cout << "gcc minor = " << __GNUC_MINOR__ << endl;
   cout << "gcc patch = " << __GNUC_PATCHLEVEL__ << endl;
-
   // check __GLIBCXX__ for macos
 #ifndef __GLIBCXX__
 #define __GLIBCXX__                                                     \
   "unkown version,please check or config your gcc path,or your OS not " \
-  "surpport."
+  "support."
 #endif
-
   cout << "libstdc++ = " << __GLIBCXX__ << endl;
 }
